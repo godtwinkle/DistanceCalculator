2f6d0ddcddd9a7638c6f24a4eb1d5a7b40b83a5e
Trần Ngọc Lâm <tnl04122001@gmail.com>
2024-02-26 00:41:18 +0700

    no message

--- src/components/DirectionsMapView.vue
+++ src/components/DirectionsMapView.vue
@@ -1,6 +1,39 @@
 <template>
-  <section class="map"></section>
+  <section class="map" ref="map"></section>
 </template>
+
+<script>
+import { EventBus } from "@/EventBus";
+export default {
+  mounted() {
+    const directionsService = new google.maps.DirectionsService();
+
+    EventBus.$on("routes-data", (routes) => {
+      const map = new google.maps.Map(this.$refs["map"], {
+        center: new google.maps.LatLng(21.028511, 105.804817),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+      });
+      routes.forEach(({ origin, destination }) => {
+        directionsService.route(
+          {
+            origin: origin.address,
+            destination: destination.address,
+            travelMode: "DRIVING",
+          },
+          (response, status) => {
+            if (status === "OK") {
+              const directionsRenderer = new google.maps.DirectionsRenderer();
+              directionsRenderer.setDirections(response);
+              directionsRenderer.setMap(map);
+            }
+          }
+        );
+      });
+    });
+  },
+};
+</script>
 
 <style scoped>
 .map {
--- src/components/RouteList.vue
+++ src/components/RouteList.vue
@@ -1,10 +1,46 @@
 <template>
   <section class="route-list-view">
-    <h1>Route List</h1>
+    <div class="route-list-view-header">
+      <h3>Danh sách tuyến đường</h3>
+    </div>
+    <select @change="sortRoute($event)">
+      <option selected disabled>Sắp xếp</option>
+      <optgroup label="Khoảng cách">
+        <option value="distance-asc">Ngắn</option>
+        <option value="distance-desc">Dài</option>
+      </optgroup>
+      <optgroup label="Thời gian">
+        <option value="duration-desc">Chậm</option>
+        <option value="duration-asc">Nhanh</option>
+      </optgroup>
+    </select>
+    <button class="ui button show-all" @click="showAllRoutesButtonPressed">
+      Show All
+    </button>
+    <div
+      class="item"
+      v-for="route in routes"
+      :key="route.id"
+      @click="routeItemPressed()"
+    >
+      <div>
+        <i class="marker alternate icon"></i>
+        {{ route.origin.address }}
+      </div>
+      <div>
+        <i class="flag checkered icon"></i>
+        {{ route.destination.address }}
+      </div>
+      <div class="ui label small">{{ route.distance.text }}</div>
+      <div class="ui label small">{{ route.duration.text }}</div>
+    </div>
   </section>
 </template>
 
 <script>
+import firebase from "firebase";
+import { EventBus } from "@/EventBus";
+
 export default {
   data() {
     return {
@@ -15,13 +51,40 @@ export default {
   created() {
     const db = firebase.firestore();
     db.collection("routes").onSnapshot((snap) => {
+      this.routes = [];
       snap.forEach((doc) => {
-        this.routes = [];
         let route = doc.data();
         route.id = doc.id;
         this.routes.push(route);
       });
     });
+  },
+
+  methods: {
+    sortRoute(e) {
+      let value = e.target.value.split("-");
+      let sortName = value[0];
+      let sortValue = value[1];
+
+      const db = firebase.firestore();
+      db.collection("routes")
+        .orderBy(sortName + ".value", sortValue)
+        .get()
+        .then((snap) => {
+          this.routes = [];
+          snap.forEach((doc) => {
+            let route = doc.data();
+            route.id = doc.id;
+            this.routes.push(route);
+          });
+        });
+    },
+    routeItemPressed(route) {
+      EventBus.$emit("routes-data", [route]);
+    },
+    showAllRoutesButtonPressed() {
+      EventBus.$emit("routes-data", this.routes);
+    },
   },
 };
 </script>
@@ -33,5 +96,22 @@ export default {
   position: relative;
   width: 260px;
   background-color: white;
+}
+
+.route-list-view-header {
+  padding: 10px;
+}
+
+.item {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.item:hover {
+  background-color: rgb(0, 0, 0, 0.1);
+}
+
+.show-all {
+  padding: 4px 10px;
 }
 </style>
